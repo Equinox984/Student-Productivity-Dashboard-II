a891ed0693e439f9606c33b9db2bb0c23f504913
Equinox984 <lopezkris984@proton.me>
2026-01-31 21:30:51 -0600

    Add utilities module with menu and task messages

---
+++ utilities.py
@@ -0,0 +1,47 @@
+"""Utilities Module"""
+
+# SIMPLE UTILITIES
+separator = "==================================\n"
+
+empty_task = """
+=========================================
+====== You don't have any tasks!!! ======
+-----------------------------------------\n"""
+
+empty_grades = """
+=========================================
+====== You don't have any grades!!! =====
+-----------------------------------------\n"""
+
+
+# EDUDASH MENUS
+taskman_menu = """
+========================================
+===== Welcome to the Task Manager! =====
+----------------------------------------
+
+1 - Task Statistics
+2 - Display Tasks
+3 - Add Tasks
+4 - Main Menu\n
+->  """
+
+grademan_menu = """\n
+=========================================
+===== Welcome to the Grade Tracker! =====
+-----------------------------------------
+
+1 - Display Grades
+2 - Add Grades
+3 - Main Menu\n
+->  """
+
+edudash_menu = """
+==================================
+====== Equinox EduDashboard ======
+----------------------------------
+
+1 - Task Manager
+2 - Grade Tracker
+3 - Exit\n
+->  """
